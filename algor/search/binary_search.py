901ed966ee1e3ab82d9a167bc3339222ac743f0d
kairat <agoodprogramist@gmail.com>
2019-01-03 06:24:07 +0600

    description of iterative bs
    algorithm: binary_search

--- algor/search/binary_search.py
+++ algor/search/binary_search.py
@@ -1,4 +1,9 @@
 def binary_search(array, target):
+    """
+Iterative binary search
+
+Complexity: O(log n)
+    """
     li, hi = 0, len(array) - 1
     while li <= hi:
         mi = (li + hi) // 2
