bebfbbe9840ef81e8beed376b4bfcb2dc6cc1eb0
kairat <agoodprogramist@gmail.com>
2019-01-03 07:29:23 +0600

    binary search tests

---
+++ tests/test_search.py
@@ -0,0 +1,22 @@
+import unittest
+
+from algor.search import (
+    binary_search, recur_binary_search
+)
+
+
+class TestSuit(unittest.TestCase):
+
+    def test_binary_search(self):
+        array = [1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6]
+        self.assertEqual(10, binary_search(array, 5))
+        self.assertEqual(11, binary_search(array, 6))
+        self.assertEqual(-1, binary_search(array, 7))
+        self.assertEqual(-1, binary_search(array, -1))
+
+    def test_recur_binary_search(self):
+        array = [1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 6]
+        self.assertEqual(10, recur_binary_search(array, 5, 0, 11))
+        self.assertEqual(11, recur_binary_search(array, 6, 0, 11))
+        self.assertEqual(-1, recur_binary_search(array, 7, 0, 11))
+        self.assertEqual(-1, recur_binary_search(array, -1, 0, 11))
